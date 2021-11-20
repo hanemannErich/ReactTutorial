1d49674954804555f5a35c3331bc3f38b29d1da3
Erich Hanemann Jr <erich_juninho@hotmail.com>
2021-11-20 16:43:45 -0300

    background

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#aaa',
+    backgroundColor: '#eee4',
     alignItems: 'center',
     justifyContent: 'center',
   },
